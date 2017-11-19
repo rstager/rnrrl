20bd66a21512ce1c4647f34ceb5fb95a79bdac33
rstager <roger@stager.us>
2017-11-19 12:06:46 -0800

    Rollout: Dont set done if step exceeded max count. discounted_future: dont zero reward if last step was not done Prioritized memory: added updater parameter to update priorites on each sample cleanup: obsact and obsact1 Memory: added summary string Memory: added episodes1 to return obs0,a0,r0,done,obs1

--- rl.py
+++ rl.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
 import random
 import numpy as np
+from sympy import ceiling, ln
+
 import gym
 
 from rnr.segment_tree import SumSegmentTree
@@ -15,6 +17,7 @@ class ExperienceMemory():
             self.md = None
             self.episodes=episodes
             self.eidx=None
+
 
         def record_reset(self, obs0):
             self.md,_ = self.memory._record(None, obs0, None, None, self.md)
@@ -33,7 +36,14 @@ class ExperienceMemory():
         self.didx=-1
         self.buffer=[]
         self.i_episode=0
+        self.writes = 0
+        self.reads = 0
+        self.wdones = 0
+        self.rdones = 0
 
+    def summary(self):
+        return "writes {} dones {} sampled reads {} dones {}".format(self.writes, self.wdones, self.reads,
+                                                                    self.rdones)
     def recorder(self,episodes=None):
         return ExperienceMemory.Recorder(self,episodes)
 
@@ -58,9 +68,14 @@ class ExperienceMemory():
         else:
             self.buffer[self.didx]=MemEntry(action, obs1, reward, done, md)
         md = MetaData(md.episode, md.step + 1, self.didx)
+        self.writes+=1
+        if done:
+            self.wdones+=1
         return md, ridx
 
-    def episodes(self,idxs,terminus=False):
+
+
+    def _episodes(self, idxs,terminus):
         for idx in idxs:
             episode=[]
             i_episode=self.buffer[idx].metadata.episode
@@ -74,6 +89,13 @@ class ExperienceMemory():
                 idx=prev
             yield self._np_experience(episode,terminus)
 
+    def episodes(self,idxs): # without obs1
+        return self._episodes(idxs,False)
+
+    def episodes1(self,idxs): # return include observation1
+        gen=self._episodes(idxs,terminus=True)
+        for obs0,a0,r0,done in gen:
+            yield obs0[:-1],a0[:-1],r0[:-1],done[:-1],obs0[1:]
 
     def _np_experience(self,idxs,terminus=False): # terminus includes the observation after the done
         batchsz=len(idxs)+(1 if terminus else 0)
@@ -87,6 +109,7 @@ class ExperienceMemory():
             Saction[idx]=self.buffer[s].action
             Sreward[idx]=self.buffer[s].reward
             Sdone[idx]=self.buffer[s].done
+
         if terminus:
             Sobs[-1]=self.buffer[idxs[-1]].obs1
             Saction[-1]=None
@@ -94,8 +117,7 @@ class ExperienceMemory():
             Sdone[-1]=None
         return Sobs,Saction,Sreward,Sdone
 
-
-    def obs1generator(self,batch_size=32,oversample_done=None,showdone=False):
+    def obs1generator(self,batch_size=32,showdone=False,sample=None,indexes=[]):
         assert len(self.buffer) > 0
         while True:
             obs0 = []
@@ -103,11 +125,13 @@ class ExperienceMemory():
             a0=[]
             r0=[]
             done=[]
+            del indexes[:]
             for cnt in range(batch_size):
                 while True:
-                    idx=random.randrange(len(self.buffer))
+                    idx=sample() if sample is not None else random.randrange(len(self.buffer))
                     if self.buffer[idx].metadata.prev != -1:
                         break
+                indexes.append(idx)
                 r=self.buffer[idx]
                 if idx >= self.didx and r.metadata.prev < self.didx:
                     continue  # skip events that span the fill point
@@ -118,42 +142,12 @@ class ExperienceMemory():
                     r0.append(r.reward)
                     done.append(r.done)
                     obs0.append(pr.obs1)
+                    self.reads += 1
+                    if r.done:
+                        self.rdones+=1
 
             yield np.array(obs0),np.array(a0),np.expand_dims(np.array(r0),axis=-1),np.array(obs1),np.expand_dims(np.array(done),axis=-1)
 
-    def _obsact(self):
-        obs0=[]
-        a0=[]
-        for r in self.buffer:
-            obs0.append(r.data.obs)
-            a0.append(r.data.action)
-        return np.array(obs0),np.array(a0)
-
-    def _obs1act(self):
-        obs0=[]
-        a0=[]
-        r0=[]
-        obs1=[]
-        done=[]
-        for idx,r in enumerate(self.buffer):
-            if idx>=self.didx and r.metadata.prev < self.didx:
-                continue # skip events that span the fill point
-            # because it is easier to keep a previous pointer,
-            # the last experience is sampled twice, the first is not sampled at all
-            if r.data.done: # if it is the end, use this sample
-                a0.append(r.data.action)
-                r0.append(r.data.reward)
-                obs0.append(r.data.obs)
-                obs1.append(np.zeros_like(r.data.obs))
-                done.append(r.data.done)
-            if r.metadata.prev != -1: #sample is first in episode
-                obs1.append(r.data.obs)
-                pr=self.buffer[r.metadata.prev]
-                a0.append(pr.data.action)
-                r0.append(pr.data.reward)
-                obs0.append(pr.data.obs)
-                done.append(pr.data.done)
-        return np.array(obs0),np.array(a0),np.array(r0),np.array(obs1),np.array(done)
 
     def save(self,filename):
         import pickle
@@ -164,47 +158,49 @@ class ExperienceMemory():
         return pickle.load( open(filename, "rb"))
 
 class PrioritizedMemory(ExperienceMemory):
-    def __init__(self,sz=10000):
-        super().__init(self,sz)
-        self._it_sum=SumSegmentTree()
+    def __init__(self,max_priority=1,sz=10000,alpha=1,updater=None):
+        super().__init__(sz)
+        self._max_priority=max_priority
+        self._alpha=alpha
+        sz2=2**np.math.ceil(np.math.log(sz, 2))
+        self._it_sum=SumSegmentTree(sz2)
+        self.nb_entries=0
+        self.updater=updater
 
-    class Recorder(ExperienceMemory.Recorder):
-        def __init__(self, memory):
-            super().__init__(self,memory)
+    def summary(self):
+        return "{} {}".format(super().summary(),self._it_sum.sum()/self.nb_entries)
 
-        def record(self, experience,priority=0):
-            idx=super().record(experience, self.md)
-            self.memory._it_sum[idx] = priority
+    def _record(self, action, obs1, reward, done, md):
+        md, ridx= super()._record(action,obs1,reward,done,md)
+        if ridx is not None:
+            self._it_sum[ridx] = self._max_priority ** self._alpha
+            self.nb_entries=max(self.nb_entries,ridx+1)
+        return md,ridx
 
-    def recorder(self):
-        return PrioritizedMemory.Recorder(self)
-
-    def sample_episode(self, episode_idx=None):
-        pass
-
-    def sample(self):
-        pass
-
-    def append(self, *args, **kwargs):
-        """See ReplayBuffer.store_effect"""
-        idx=super().append(*args,**kwargs)
-        self._it_sum[idx] = self._max_priority ** self._alpha
-        self._it_min[idx] = self._max_priority ** self._alpha
-
-    def _sample_proportional(self, batch_size):
-        res = []
-        for _ in range(batch_size):
-            # TODO(szymon): should we ensure no repeats?
-            mass = random.random() * self._it_sum.sum(0, self.nb_entries - 1)
-            idx = self._it_sum.find_prefixsum_idx(mass)
-            res.append(idx)
-        return res
+    def obs1generator(self,batch_size=32,showdone=False,):
+        def sample():
+            return self._sample_index() # self caught in closure
+        #return super().obs1generator(batch_size=batch_size,showdone=showdone,sample=sample)
+        indexes=[]
+        gen=super().obs1generator(batch_size=batch_size,sample=sample,indexes=indexes)
+        for obs0, a0, r0, obs1, done in gen:
+            yield obs0, a0, r0, obs1, done
+            if self.updater is not None:
+                priorities=self.updater(obs0,a0,r0,obs1,done)
+                if priorities is not None:
+                    #assert len(indexes) == len(priorities)
+                    for (priority,idx) in zip(priorities,indexes):
+                        self._it_sum[idx] = priority ** self._alpha
+                    #print("sample {} base {} done {}".format(np.mean(priorities), self._it_sum.sum() / self.nb_entries,np.sum(done)))
+    def _sample_index(self):
+        mass = random.random() * self._it_sum.sum(0, self.nb_entries - 1)
+        idx = self._it_sum.find_prefixsum_idx(mass)
+        return idx
 
 
-
-def discounted_future(reward,gamma):
+def discounted_future(reward,gamma,done=True):
         df=np.copy(reward)
-        last = 0
+        last = reward[-1]/(1-gamma) if not done else 0
         for i in reversed(range(df.shape[0])):
             df[i] += gamma * last
             last = df[i]
@@ -233,12 +229,13 @@ class EnvRollout:
         self._instances=[gym.make(envname) for x in range(nenv)]
         self.env = self._instances[0]
         self.nenv=nenv
+        self.max_steps=getattr(self.env,'_max_episode_steps',1e6)-1
 
     def rollout(self, policy, memory=None,exploration=None,visualize=False,nepisodes=None,nsteps=None,state=None,episodes=None):
         bobs = []
         recorders=[]
         step_cnt,episode_cnt=0,0
-
+        env_nsteps=[0]*self.nenv
         if memory is None:
             memory=ExperienceMemory()
         n= nepisodes if nepisodes is not None and nepisodes<len(self._instances) else len(self._instances)
@@ -250,7 +247,7 @@ class EnvRollout:
                 tobs = env.env.set_state(state)
             bobs.append(tobs)
 
-        while (nepisodes is not None and episode_cnt<nepisodes) or (nsteps is not None and step_cnt<nsteps):
+        while (nepisodes is None or episode_cnt<nepisodes) and (nsteps is None or step_cnt<nsteps):
             if policy is None:
                 acts=[e.env.controller() for e in self._instances]
             else:
@@ -265,17 +262,21 @@ class EnvRollout:
                 if noise is not None:
                     action += noise
                 tobs,reward,done,_=env.step(action)
-                recorder.record_step(action,tobs, reward, done)
+                if not (done and  env_nsteps[i_env] == self.max_steps):  # dont record timed out step
+                    recorder.record_step(action,tobs, reward, done)
                 if visualize and i_env==0:
                     env.render()
                 if done:
                     tobs=env.reset()
                     recorder.record_reset(tobs)
+                    env_nsteps[i_env]=0
                     if state is not None:
                         tobs=env.env.set_state(state)
                     if exploration is not None:
                         exploration[i_env].reset()
                     episode_cnt+=1
+                else:
+                    env_nsteps[i_env] += 1
                 bobs[i_env]=tobs
                 step_cnt+=1
                 if nsteps is not None and step_cnt>=nsteps:
