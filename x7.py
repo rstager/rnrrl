1795f81a1af13cc8ac6dff89bb55d7559bea39d5
rstager <roger@stager.us>
2017-12-15 15:16:07 -0800

    added tip angle check for done. added cosine/sine

--- x7.py
+++ x7.py
@@ -22,9 +22,10 @@ from math import log, cos, sin, sqrt
 import numpy as np
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from keras import Input, Model, regularizers
-from keras.layers import Dense, concatenate, Activation, BatchNormalization
+from keras.layers import Dense, concatenate, Activation, BatchNormalization, Dropout
 
-from callbacks import ActorCriticEval, PlotDist, PltQEval, SaveModel, PltObservation, ActorCriticRollout, PltQEval2
+from callbacks import ActorCriticEval, PlotDist, PltQEval, SaveModel, PltObservation, ActorCriticRollout, PltQEval2, \
+    ParameterSchedule
 from rl import EnvRollout, PrioritizedMemory, TD_q
 from rlagents import DDPGAgent
 from rnr.gym import rnrenvs
@@ -33,7 +34,7 @@ from rnr.util import kwargs
 
 import matplotlib.pyplot as plt
 
-def make_models(env,reg=1e-4):
+def make_models(env,reg=1e-2):
     ain = Input(shape=env.action_space.shape, name='action')
     oin = Input(shape=env.observation_space.shape, name='observeration')  # full observation
     common_args = kwargs(kernel_initializer='glorot_normal',
@@ -55,10 +56,10 @@ def make_models(env,reg=1e-4):
 
     x = oin
     x = concatenate([x, ain], name='sensor_goal_action')
-    x = Dense(32, **common_args)(x)
+    x = Dense(32, kernel_regularizer=regularizers.l2(reg),**common_args)(x)
     #x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Dense(32, **common_args)(x)
+    x = Dense(32,kernel_regularizer=regularizers.l2(reg), **common_args)(x)
     #x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = Dense(128, kernel_regularizer=regularizers.l2(reg),**common_args)(x)
@@ -82,24 +83,29 @@ def objective(kwargs):
         tdq = TD_q(agent.target_actor, agent.target_critic, agent.gamma, obs1, r0, done)
         q0 = agent.target_critic.predict([obs0,a0])
         if clip_tdq is not None:
-            tdq = np.clip(tdq, clip_tdq / (1 - gamma), 0)
+            tdq = np.clip(tdq, clip_tdq / (1 - agent.gamma), 0)
         epsilon=0.00001
         priority=np.abs((q0-tdq)/(tdq+epsilon))
         priority=np.clip(priority,0.0001,1)
         return priority.squeeze(axis=-1)
 
-    eval=ActorCriticEval(cluster,agent.target_actor,agent.target_critic,gamma)
+    eval=ActorCriticEval(cluster,agent.target_actor,agent.target_critic,agent)
     callbacks=[]
     callbacks.append(eval)
     # callbacks.append(PltQEval(cluster, gamma, [('target', agent.target_actor, agent.target_critic),
     #                                            ('ddpg', agent.actor, agent.critic)], title="RL eval",fignum=1))
-    callbacks.append(PlotDist(cluster, eval.hist, title="actor/critic training trends",fignum=1))
+    callbacks.append(PlotDist(cluster, eval.hist, title="actor/critic training trends",fignum=1,verbose=True))
     callbacks.append(ActorCriticRollout(cluster,agent.target_actor,agent.target_critic,
                                         [PltObservation(cluster.env, fignum=2),
-                                         PltQEval2(cluster.env,gamma,fignum=3)]))
-    callbacks.append(SaveModel(agent.target_actor,agent.target_critic,skip=10))
+                                         PltQEval2(cluster.env,agent,fignum=3)],skip=10))
+    callbacks.append(SaveModel(agent.target_actor,agent.target_critic))
+    # callbacks.append(ParameterSchedule(agent,{50:{'gamma':0.95,'lr':0.001},
+    #                                           100:{'gamma':.98},
+    #                                           150:{'gamma':.99},
+    #                                           200:{'gamma':.995}}))
+    callbacks.append(ParameterSchedule(agent,{50:{'clr':0.006,'lr':0.0006},100:{'clr':0.001,'lr':0.0001},}))
     memory = PrioritizedMemory(sz=100000,env=cluster.env,updater=qpriority)
-    agent.train(memory=memory,epochs=epochs, fignum=1, visualize=False,callbacks=callbacks,nsteps=1000)
+    agent.train(memory=memory,epochs=epochs, fignum=1, visualize=False,callbacks=callbacks,nsteps=10000)
 
     reward=eval.hist['reward']
     r1=np.array(reward)
@@ -120,8 +126,9 @@ class ComplexSliderEnv(SliderEnv):
         self.ndim=kwargs.get('ndim',1)
         self.links=[1.0,0.7,0.3]
         ospace=2 if self.ndim!=3 else 3
-        self.observation_space = spaces.Box(low=np.array([0]*self.ndim+[-1]*self.ndim+[-self.ndim]*ospace),
-                                           high=np.array([1]*self.ndim+[1]*self.ndim+[self.ndim]*ospace))
+        # velocities, goalxyt,sintheta,costheta
+        self.observation_space = spaces.Box(low=np.array([-1]*self.ndim+[-self.ndim]*ospace+[-1]*self.ndim*2),
+                                           high=np.array([1]*self.ndim+[self.ndim]*ospace+[1]*self.ndim*2))
 
     def _angles_to_coord(self,angles):
         # simulate nservo reward
@@ -134,7 +141,7 @@ class ComplexSliderEnv(SliderEnv):
             ys.append(ys[-1] + l * sin(ts[-1]))
         del ts[0]
         if self.ndim==3:
-            return np.array([xs[-1],ys[-1],ts[-1]])
+            return np.array([xs[-1],ys[-1],np.mod(ts[-1]/np.pi+11,2)-1])
         else:
             return np.array([xs[-1], ys[-1]])
 
@@ -142,20 +149,24 @@ class ComplexSliderEnv(SliderEnv):
         obs,reward,done,info=super()._step(u)
         obs=super()._get_obs()
         newobs=self._get_obs()
+        # if self.hit_limit:
+        #     print("Hit limit {}".format(obs))
         #tip=self._angles_to_coord(obs[:self.ndim]*np.pi)
         #newgoal=self._angles_to_coord(obs[2*self.ndim:3*self.ndim]*np.pi)
         tip=self._angles_to_coord(obs[0:self.ndim])
         goal=self._angles_to_coord(obs[2 * self.ndim:3 * self.ndim])
         d=np.sqrt(np.sum(np.square(tip[:2]-goal[:2])))
         newreward= -0.5*(d**2)
-        if self.ndim==3:
-            newreward+= -0.1*(sin((tip[-1]-goal[-1])*np.pi)**2)
-        # if self.hit_limit:
-        #     newreward-=5
-        if d<0.1: #override the done. There may be multiple solutions that get close enough to the goal.
+        c=0
+        # if self.ndim==3:
+        #     c=1-cos((tip[-1]-goal[-1])*np.pi)
+        #     newreward+= -0.1*(c**2)
+        if self.ndim <3 and d<0.1:
             done=True
-        if done:
-            print("tip={}, goal={}, d={} obs {} newobs {}".format(tip,goal,newreward,obs,newobs))
+        elif d<0.1 and c<0.1: #override the done. There may be multiple solutions that get close enough to the goal.
+            done=True
+        # if done:
+        #     print("tip={}, goal={}, d={} c={} obs {} newobs {}".format(tip,goal,d,c,obs,newobs))
         newreward+=-0.1*(np.sum(np.square(np.array(obs[self.ndim:self.ndim*2])))) -0.1*(np.sum(np.square(u)))-1
         return newobs,newreward,done,info # original solution
 
@@ -169,7 +180,10 @@ class ComplexSliderEnv(SliderEnv):
 
     def _get_obs(self):
         obs=super()._get_obs()
-        newobs=np.array(list(obs[0:2*self.ndim])+list(self._angles_to_coord(obs[2*self.ndim:])))
+        #newobs=np.array(list(obs[0:2*self.ndim])+list(self._angles_to_coord(obs[2*self.ndim:])))
+        newobs = np.array(list(obs[0:2 * self.ndim]) + list(self._angles_to_coord(obs[2 * self.ndim:])))
+        #add sine&cosine
+        newobs = np.hstack([newobs[self.ndim:],np.sin(newobs[0:self.ndim]*np.pi),np.cos(newobs[0:self.ndim]*np.pi)])
         return newobs
 
     def _render(self, mode='human', close=False):
@@ -210,7 +224,7 @@ def run():
     rnrenvs()
 
     trials = Trials()
-    kwargs={'ndim':2,'minx':0,'maxx':1,'wrap':False} #,'image_goal':[40,40]}
+    kwargs={'ndim':3,'minx':[-1,-1,-1],'maxx':[1,1,1],'wrap':True} #,'image_goal':[40,40]}
 
     gym.envs.register(
         id='Test-v0',
@@ -220,12 +234,16 @@ def run():
     )
     cluster = EnvRollout('Test-v0', 1)
 
+    import pickle
+    with open('envspec.p', 'wb') as f:
+        pickle.dump(cluster.env.env.spec, f)
+
     space = {
             'cluster':cluster,
             'gamma':0.9,
             'epochs': 2000,
             'tau': 1e-2,
-            'reg':1e-4,
+            'reg':1e-3,
             'lr':1e-3,
             'clr':1e-2,
             'decay':1e-6,
